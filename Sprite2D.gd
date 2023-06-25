bb001d82d78177862a910a89f79426dc08b97fc5
Kev Zettler <kevzettler@gmail.com>
2023-06-25 15:42:13 -0700

    rotation, scale, bump all works

--- Sprite2D.gd
+++ Sprite2D.gd
@@ -1,12 +1,15 @@
 extends Sprite2D
 
-var fall_velocity = 0.001;
+var fall_velocity = 0.0001;
 var mode7: Transform3D;
 var depth = Vector2(0,0);
 var rotation_angle = 0.0;
 var zaxis = Vector3(0,0,1);
 var scale_update = Vector3(1.0,1.0,1.0);
+
 var drift_update = Vector3(0.0,-0.001,0.0);
+var drift_bump_direction = Vector3();
+var drift_bump_forward = Vector3(0.0, -0.001, 0.0);
 
 func _ready():
 	var mat = get_material();
@@ -19,18 +22,23 @@ func _process(delta):
 	if Input.is_action_pressed("move_left"):
 		rotation_angle -= 1.0 * delta
 	if Input.is_action_pressed("move_down"):
-		drift_update.y = 0.01
+		drift_bump_direction = drift_bump_forward * -1
 	if Input.is_action_pressed("move_up"):
-		drift_update.y = -0.01
+		drift_bump_direction = drift_bump_forward
 		
 	mode7 = mode7.rotated(zaxis, rotation_angle);
 		
 	scale_update.x -= fall_velocity * delta;
 	scale_update.y -= fall_velocity * delta;
+	# print("scale_update", scale_update);
 	mode7 = mode7.scaled(scale_update);
 	
 	drift_update = drift_update.rotated(zaxis, rotation_angle);
+	drift_bump_direction = drift_bump_direction.rotated(zaxis, rotation_angle)
+	drift_update += drift_bump_direction;
 	mode7 = mode7.translated(drift_update);
+	
+	drift_update -= drift_bump_direction; #reset the drift bump
 	rotation_angle = 0.0; # reset rotation angle
 		
 	var mat = get_material();
