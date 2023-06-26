dcef3d32750f2940acfa076ee5cfca38fd33ef16
Kev Zettler <kevzettler@gmail.com>
2023-06-25 17:00:19 -0700

    fix rotation move it to rotation_local

--- Sprite2D.gd
+++ Sprite2D.gd
@@ -5,7 +5,7 @@ var mode7: Transform3D;
 var depth = Vector2(0,0);
 var rotation_angle = 0.0;
 var zaxis = Vector3(0,0,1);
-var scale_update = Vector3(1.0,1.0,1.0);
+var scale_update = Vector3(1,1,1.0);
 
 var drift_update = Vector3(0.0,-0.001,0.0);
 var drift_bump_direction = Vector3();
@@ -22,22 +22,24 @@ func _process(delta):
 	if Input.is_action_pressed("move_left"):
 		rotation_angle -= 1.0 * delta
 		
-	mode7 = mode7.rotated(zaxis, rotation_angle);
 	drift_bump_forward = drift_bump_forward.rotated(zaxis, rotation_angle)
 	
 	if Input.is_action_pressed("move_down"):
 		drift_bump_direction = drift_bump_forward * -1
 	if Input.is_action_pressed("move_up"):
 		drift_bump_direction = drift_bump_forward	
-		
+
+	drift_update = drift_update.rotated(zaxis, rotation_angle);
+	drift_update += drift_bump_direction;
+	mode7 = mode7.translated(drift_update);
+
+	mode7 = mode7.rotated_local(zaxis, rotation_angle);
+	
 	scale_update.x -= fall_velocity * delta;
 	scale_update.y -= fall_velocity * delta;
 	print("scale_update", scale_update);
 	mode7 = mode7.scaled(scale_update);
 	
-	drift_update = drift_update.rotated(zaxis, rotation_angle);
-	drift_update += drift_bump_direction;
-	mode7 = mode7.translated(drift_update);
 	
 	drift_update -= drift_bump_direction; #reset the drift bump
 	rotation_angle = 0.0; # reset rotation angle
